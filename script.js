14b55fbda2e12515e965013d7b68e0feb7208aad
pranajalsingh <pjal25352@gmail.com>
2025-04-04 05:32:56 +0530

    sound added

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var user_pattern = [];
 function newSequence(){
     var new_tile = randomNumber();
     pattern.push(tiles[new_tile]);
-    $("." + tiles[new_tile]).fadeOut().fadeIn();
+    setTimeout(function(){
+        $("." + tiles[new_tile]).fadeOut().fadeIn();
+    }, 200);
+    
 }
 
 function gameStart(){
@@ -22,11 +25,40 @@ function randomNumber(){
     return Math.floor(Math.random()*4);
 }
 
-// for (i=0; i<4; i++){
-//     document.querySelector("button")[i].addEventListener("click",function(){
-//         this.classList.add("clicked");
-//     });
-// }
+function checkClick(){
+    if (pattern.length === 0){
+        gameOver();
+        return false;
+    } else{
+        for (i=0; i<user_pattern.length; i++){
+            if (user_pattern[i] != pattern[i]){
+                gameOver();
+                return false;
+            }
+        }
+        if (pattern.length === user_pattern.length){
+            levelUp();
+        }
+    }
+    return true;
+}
+
+function levelUp(){
+    level++;
+    user_pattern = [];
+    $("h1").text("level " + level);
+    newSequence();
+}
+
+function gameOver(){
+    pattern = []
+    $("h1").text("Game Over at Level " + level + ". Press any Key to Restart");
+    $("body").css("background-color", "red");
+    setTimeout(function(){
+        $("body").css("background-color", "#011F3F");
+    }, 100);
+    started = false;
+}
 
 $(document).keypress(function(){
     if (!started){
@@ -35,3 +67,20 @@ $(document).keypress(function(){
     }
 });
 
+$("button").click(function(){
+    var btn = $(this).addClass("clicked");
+    setTimeout(function(){
+        btn.removeClass("clicked");
+    }, 100);
+    user_pattern.push(btn.attr("id"));
+    sound(btn.attr("id"));
+});
+
+function sound(btn){
+    if (!checkClick()){
+        var audio = new Audio("./sounds/wrong.mp3");
+    } else{
+        var audio = new Audio("./sounds/"+ btn +".mp3");
+    }
+    audio.play();
+}
